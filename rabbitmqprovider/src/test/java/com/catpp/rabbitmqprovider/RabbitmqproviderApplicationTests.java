ee3c06ffd139c405206cad324f618505bcf1a7e9
Maopp <1121266440@qq.com>
2018-11-06 11:56:37 +0800

    基于SpringBoot & RabbitMQ完成DirectExchange分布式消息多消费者消费

---
+++ rabbitmqprovider/src/test/java/com/catpp/rabbitmqprovider/RabbitmqproviderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.catpp.rabbitmqprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqproviderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ rabbitmqprovider/src/test/java/com/catpp/rabbitmqprovider/function/UserRegisterTest.java
@@ -0,0 +1,59 @@
+package com.catpp.rabbitmqprovider.function;
+
+import com.catpp.rabbitmqprovider.base.BaseTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+/**
+ * com.catpp.rabbitmqprovider.function
+ *
+ * @Author cat_pp
+ * @Date 2018/11/6
+ * @Description
+ */
+@Slf4j
+public class UserRegisterTest extends BaseTest {
+
+    @Test
+    public void testBatchUserAdd() throws Exception {
+        for (int i = 0;i < 10;i++) {
+            // 创建用户注册线程
+            Thread t = new Thread(new BatchRabbitTester(i));
+            // 启动线程
+            t.start();
+        }
+        // 等待线程执行完成
+        // 为了阻塞测试用例的结束，因为我们测试用户完成方法后会自动停止，不会去等待其他线程执行完成，所以这里我们阻塞
+        // 测试主线程来完成发送注册线程请求逻辑。
+        Thread.sleep(2000);
+    }
+
+    /**
+     * 批量注册用户测试线程类
+     */
+    class BatchRabbitTester implements Runnable {
+
+        private int index;
+
+        public BatchRabbitTester(int index) {
+            this.index = index;
+        }
+
+        @Override
+        public void run() {
+            try {
+                mockMvc.perform(MockMvcRequestBuilders.post("/provider/saveUser")
+                        .param("username", "testRabbitMQ" + index)
+                        .param("password", "123qwe")
+                        .param("nickname", "testRabbitMQ" + index))
+                        .andDo(MockMvcResultHandlers.log())
+                        .andReturn();
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
+
+}
